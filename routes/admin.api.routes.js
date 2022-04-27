291f87dc732685a5b22a6c4677b38ed45730bbc3
Abanoupjohnny <bebo_johnny@hotmail.com>
2022-04-27 20:56:07 +0200

    del

--- routes/admin.api.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-const Admin = require('../app/controller/admin.controller')
-
-// const auth = require('')
-
-router.post('/add')
-module.exports = router
